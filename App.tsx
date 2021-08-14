85e4620022ef37928c58a62aa411666392addd5d
Julio Romero <julioale21@gmail.com>
2021-08-14 13:52:11 -0300

    added and configured husky

--- App.tsx
+++ App.tsx
@@ -18,7 +18,13 @@ const App = () => {
         resizeMode="contain"
         style={styles.image}
       />
-      <DefaultButton onPress={showAlert} />
+      <DefaultButton
+        //additionalStyle={styles.button}
+        color={colors.mainOrange}
+        onPress={showAlert}
+        text="Hola"
+        textSize={24}
+      />
     </View>
   );
 };
@@ -41,6 +47,10 @@ const styles = StyleSheet.create({
   image: {
     minHeight: 100,
     width: DEVICE_WITDH * 0.5,
+  },
+  button: {
+    backgroundColor: 'green',
+    width: 50,
   },
 });
 
