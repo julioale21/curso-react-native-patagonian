01d8bba4639d2a50fbcfba383e8d0241e136a8be
Julio Romero <julioale21@gmail.com>
2021-08-25 17:20:54 -0300

    created 'Header' component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { SafeAreaView, View, TouchableOpacity } from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import Separator from '../Separator';
+import { colors } from '../../utils/theme';
+import Typography from '../Typography';
+import styles from './styles';
+
+import { goBack } from '../../navigation/controls';
+
+interface Props {
+  onPressBackButton?: () => void;
+  onPressRightButton?: () => void;
+  RightSideComponent?: React.FC;
+  showBackButton?: boolean;
+  title: string;
+}
+
+const Header = ({
+  onPressBackButton,
+  onPressRightButton,
+  RightSideComponent,
+  showBackButton,
+  title,
+}: Props) => {
+  return (
+    <>
+      <SafeAreaView />
+      <View style={styles.mainContainer}>
+        {showBackButton ? (
+          <TouchableOpacity
+            onPress={onPressBackButton}
+            style={styles.sideButtonContainer}>
+            <MaterialIcon
+              name="navigate-before"
+              size={35}
+              color={colors.black}
+            />
+          </TouchableOpacity>
+        ) : (
+          <Separator isHorizontal size={40} />
+        )}
+        <View style={styles.titleContainer}>
+          <Typography variant="bold" size={17}>
+            {title}
+          </Typography>
+        </View>
+        {RightSideComponent ? (
+          <TouchableOpacity
+            onPress={onPressRightButton}
+            style={styles.sideButtonContainer}>
+            <RightSideComponent />
+          </TouchableOpacity>
+        ) : (
+          <Separator isHorizontal size={40} />
+        )}
+      </View>
+    </>
+  );
+};
+
+Header.defaultProps = {
+  onPressBackButton: goBack,
+  onPressRightButton: () => {},
+  showBackButton: true,
+};
+
+export default Header;
